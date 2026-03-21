ee5afa3a6a19a63fe89162a42e5e1055dd683b3e
elprofesor <rrazvan.radeanu@gmail.com>
2026-03-21 07:46:52 +0200

    split into html, js, css

---
+++ script.js
@@ -0,0 +1,22 @@
+;(function () {
+  'use strict';
+
+  var btn = document.querySelector('.btn-theme');
+  var sun = btn.querySelector('.icon-sun');
+  var moon = btn.querySelector('.icon-moon');
+
+  function applyTheme(light) {
+    document.documentElement.classList.toggle('light', light);
+    sun.style.display = light ? 'none' : '';
+    moon.style.display = light ? '' : 'none';
+    try { localStorage.setItem('catalogue-theme', light ? 'light' : 'dark'); } catch (e) { /* private browsing */ }
+  }
+
+  var saved = null;
+  try { saved = localStorage.getItem('catalogue-theme'); } catch (e) { /* private browsing */ }
+  applyTheme(saved === 'light');
+
+  btn.addEventListener('click', function () {
+    applyTheme(!document.documentElement.classList.contains('light'));
+  });
+})();
